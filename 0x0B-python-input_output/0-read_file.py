551ed9346d0e7c88212bdad99f762d91ac36bbe4
Ephraim67 <jonathaneuphrem@gmail.com>
2024-04-16 21:27:53 -0400

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function that reads a text file (UTF8)and print it to stdout"""
 
+
 def read_file(filename=""):
     """function that reads a file"""
     with open(filename, encoding="utf-8") as f:
