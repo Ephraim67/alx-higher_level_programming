b62f411f84c5f67319ebc249778b99841f75ac07
Ephraim67 <jonathaneuphrem@gmail.com>
2024-04-24 07:42:44 -0400

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,7 +56,7 @@ class Rectangle(Base):
         """set the value of the width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -65,7 +65,7 @@ class Rectangle(Base):
         """set the value of the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be >=0")
         self.__height = value
 
