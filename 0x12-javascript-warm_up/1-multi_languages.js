8aea916456d7c1873575e3b8b9d7b43f3f793ef7
Ephraim67 <jonathaneuphrem@gmail.com>
2025-07-15 19:44:50 +0100

    Adding this to make corrections.

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVar_1 = "C is fun";
+const myVar_2 = "Python is cool";
+const myVar_3 = "Javascript is amazing";
+
+console.log(myVar_1);
+console.log(myVar_2);
+console.log(myVar_3);
