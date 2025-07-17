66fe2c30fd88c153a93df5900f4c0ca055b08f9a
Ephraim67 <jonathaneuphrem@gmail.com>
2025-07-17 00:10:02 -0400

    Fix 1-multi_languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 
 const myVar_1 = "C is fun";
 const myVar_2 = "Python is cool";
-const myVar_3 = "JavaScript is amazing";
+const myVar_3 = "Javascript is amazing";
 
 console.log(myVar_1);
 console.log(myVar_2);
