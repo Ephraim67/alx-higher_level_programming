3ea789ad69e3327f3fcd1978deb0dc2fbfa1c5e9
Ephraim67 <jonathaneuphrem@gmail.com>
2025-10-02 22:22:07 -0400

    test

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const myVar_1 = "C is fun";
-const myVar_2 = "Python is cool";
-const myVar_3 = "Javascript is amazing";
+const myVar_1 = 'C is fun';
+const myVar_2 = 'Python is cool';
+const myVar_3 = 'Javascript is amazing';
 
 console.log(myVar_1);
 console.log(myVar_2);
